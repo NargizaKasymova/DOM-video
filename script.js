d06aa8a8cd6ee479012ded34959282d2fea8de55
Nargiza <n.kasymova2021@gmail.com>
2021-07-17 23:43:59 +0600

    DOM

--- script.js
+++ script.js
@@ -1,18 +1,65 @@
-console.log(navigator.userAgent)
+// const windowWidth = window.innerWidth
+// console.log(`Ширина окна браузера: ${window.innerWidth}px`)
 
-if(navigator.userAgent.includes('Chrome')) {
-    console.log('Браузер Chrome')
-} else if(navigator.userAgent.includes('Firefox')){
-    console.log('Браузер Firefox')
-} 
-    console.log(navigator.platform)
 
-// console.log(location.href)
-// location.href = "https://fls.guru"
+// console.log(navigator.userAgent)
 
-history.back()
-history.forward()
+// if(navigator.userAgent.includes('Chrome')) {
+//     console.log('Браузер Chrome')
+// } else if(navigator.userAgent.includes('Firefox')){
+//     console.log('Браузер Firefox')
+// } 
+//     console.log(navigator.platform)
 
-alert('Hello')
-const confirmResult = confirm("Want u to know css?")
-console.log(confirmResult)
+// // console.log(location.href)
+// // location.href = "https://fls.guru"
+
+// history.back()
+// history.forward()
+
+// alert('Hello')
+// const confirmResult = confirm("Want u to know css?")
+// console.log(confirmResult)
+
+// const promptResult = prompt('Кто ты по жизни?')
+// console.log(promptResult)
+
+// const htmlElement = document.documentElement
+// const headElement = document.head
+const bodyElement = document.body
+// console.log(htmlElement)
+// console.log(headElement)
+// console.log(bodyElement)
+
+// const firstChildNode = bodyElement.firstChild
+// const lastChildNode = bodyElement.lastChild
+
+// const firstChildNode = bodyElement.firstElementChild
+// const lastChildNode = bodyElement.lastElementChild
+
+// console.log(firstChildNode)
+// console.log(lastChildNode)
+
+// const childNodes = bodyElement.childNodes
+// console.log(childNodes)
+
+// console.log(bodyElement.hasChildNodes())
+
+// for (let node of childNodes) {
+//     console.log(node)
+// }
+
+// const previousSiblingNode = bodyElement.previousSibling
+// const nextSiblingNode = bodyElement.nextSibling
+// const parentNode = bodyElement.parentNode
+
+// const previousSiblingNode = bodyElement.previousElementSibling
+// const nextSiblingNode = bodyElement.nextElementSibling
+// const parentNode = bodyElement.parentNode
+
+// console.log(previousSiblingNode)
+// console.log(nextSiblingNode)
+// console.log(parentNode)
+
+const bodyChildren = bodyElement.children
+console.log(bodyChildren)
