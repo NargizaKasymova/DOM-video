7815d750e46dd46bf55d56682ea373ecab9b2056
Nargiza <n.kasymova2021@gmail.com>
2021-07-17 23:05:10 +0600

    DOM video

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log(navigator.userAgent)
+
+if(navigator.userAgent.includes('Chrome')) {
+    console.log('Браузер Chrome')
+} else if(navigator.userAgent.includes('Firefox')){
+    console.log('Браузер Firefox')
+} 
+    console.log(navigator.platform)
+
+// console.log(location.href)
+// location.href = "https://fls.guru"
+
+history.back()
+history.forward()
+
+alert('Hello')
+const confirmResult = confirm("Want u to know css?")
+console.log(confirmResult)
